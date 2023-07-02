b42e0358d2794694fcde00765bfac51f090371dd
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-07-02 10:53:31 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,9 +1,10 @@
 from bot import Bot
 from pyrogram import Client, filters
+from database.database import add_group
 
 @Bot.on_message(filters.new_chat_members)
 async def new_group(client, message):
     for member in message.new_chat_members:
         if member.id == client.get_me().id:
-   
+            await add_group(message.chat.id)
             await message.reply_text("Thanks for adding me!")￼Enter
